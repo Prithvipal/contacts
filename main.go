377c6d1b724ed51a005da475938c9fe282fd586f
Prithvipal Singh <prithvirathore99@gmail.com>
2021-03-28 19:39:36 +0530

    returning dummy json in hello API

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,5 +14,21 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Prithvi!!! How are you??"))
+	c := contact{
+		Name:        "Prithvi",
+		Address:     "Pune",
+		PhoneNumber: []int{19292, 3040404},
+		Owners:      []string{"Prithvipal", "Prithviraj"},
+	}
+	data, _ := json.Marshal(c)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+	// w.Write([]byte("Hello Prithvi!!! How are you??"))
+}
+
+type contact struct {
+	Name        string   `json:"name"`
+	Address     string   `json:"address"`
+	PhoneNumber []int    `json:"phone_number"`
+	Owners      []string `json:"owners"`
 }
